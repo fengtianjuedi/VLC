813a7ab1ad875625c98c8af47d7672f67a5e380b
wufeng <wufeng13524474117.163.com>
2020-07-22 17:45:37 +0800

    第一次提交。吴峰

---
+++ VLCTest/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace VLCTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+                if(this.libVLC != null)
+                    this.libVLC.Dispose();
+                if (this.mp != null)
+                    this.mp.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoView1.Location = new System.Drawing.Point(0, 0);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(437, 311);
+            this.videoView1.TabIndex = 0;
+            this.videoView1.Text = "videoView1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 311);
+            this.Controls.Add(this.videoView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+
+    }
+}
+
---
+++ VLCTest/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+using LibVLCSharp.WinForms;
+
+namespace VLCTest
+{
+    public partial class Form1 : Form
+    {
+        public LibVLC libVLC;
+        public MediaPlayer mp;
+
+        public Form1()
+        {
+            if (!DesignMode)
+            {
+                Core.Initialize();
+            }
+            InitializeComponent();
+            libVLC = new LibVLC();
+            mp = new MediaPlayer(libVLC);
+            videoView1.MediaPlayer = mp;
+        }
+
+        /// <summary>
+        /// 窗体加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mp.Playing += mp_Playing;
+            mp.Buffering += mp_Buffering;
+            mp.Opening += mp_Opening;
+            mp.EncounteredError += mp_EncounteredError;
+            
+            mp.SetVideoTitleDisplay(Position.Center, 5000);
+            bool result = mp.Play(new Media(libVLC, "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115j.mov", FromType.FromLocation));
+            if (!result)
+                MessageBox.Show("播放失败");
+        }
+
+        void mp_EncounteredError(object sender, EventArgs e)
+        {
+            MessageBox.Show("无信号");
+        }
+
+        void mp_Opening(object sender, EventArgs e)
+        {
+            MessageBox.Show("连接中");
+        }
+
+        void mp_Buffering(object sender, MediaPlayerBufferingEventArgs e)
+        {
+            if (e.Cache == 100)
+                MessageBox.Show("缓冲成功");
+            else
+                Console.WriteLine("缓冲中");
+        }
+
+        void mp_Playing(object sender, EventArgs e)
+        {
+            MessageBox.Show("连接成功");
+        }
+    }
+}
